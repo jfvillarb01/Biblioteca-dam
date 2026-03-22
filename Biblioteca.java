2e3bfea3f0813fda52e83e13576b7cdfca171276
nando <jfvillarb01@educarex.es>
2026-03-22 12:28:38 +0100

    Agregar libro

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,3 +1,13 @@
 public class Biblioteca {
     Libro[] libros = new Libro[10];
+
+    public void agregarLibro(Libro libro) {
+        for (int i = 0; i < libros.length; i++) {
+            if (libros[i] == null) {
+                libros[i] = libro;
+                System.out.println("Libro agregado: " + libro.titulo);
+                break;
+            }
+        }
+    }
 }
