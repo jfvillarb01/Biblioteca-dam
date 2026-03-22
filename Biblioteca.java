f2e9b8def8bd4b93ee3cc6902254093e66ff15fa
nando <jfvillarb01@educarex.es>
2026-03-22 12:45:53 +0100

    Prestar libro

--- Biblioteca.java
+++ Biblioteca.java
@@ -13,10 +13,21 @@ public class Biblioteca {
 
     public void mostrarLibros() {
     System.out.println("Catálogo de la biblioteca:");
-    for (Libro libro : libros) {
+        for (Libro libro : libros) {
         if (libro != null) {
             System.out.println("• " + libro.titulo + " - " + libro.autor);
         }
+        }    
     }
-}
+
+    public void prestarLibro(String titulo) {
+        for (Libro libro : libros) {
+        if (libro != null && libro.titulo.equals(titulo)) {
+            System.out.println("📕 Libro prestado: " + titulo);
+            return;
+        }
+        }
+    System.out.println("Libro no encontrado");
+    }
+
 }
