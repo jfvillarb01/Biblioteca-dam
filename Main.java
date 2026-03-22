0151e0b4ed355e055a443e6942133882b2b51340
nando <jfvillarb01@educarex.es>
2026-03-22 12:32:40 +0100

    Main

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
+
+        biblioteca.agregarLibro(new Libro("1984", "George Orwell"));
+        biblioteca.agregarLibro(new Libro("El Quijote", "Cervantes"));
+
+        biblioteca.mostrarLibros();
     }
 }
